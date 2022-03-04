8d4ca21976589121c9bbf6e044cc8dc4b38da448
Wahyu Adi Nugroho <wahyuadinugrohoasinan@gmail.com>
2022-03-04 23:11:09 +0700

    Menambahkan fungsi modulo

--- library.py
+++ library.py
@@ -1,10 +1,12 @@
 from globals import *
 import sys
 
+
 class Calculator:
-    def __init__(self, angka1 = nil1, angka2=nil2):
+    def __init__(self, angka1=nil1, angka2=nil2):
         self.angka1 = angka1
         self.angka2 = angka2
+
     def add(self):
         return self.angka1 + self.angka2
 
@@ -22,16 +24,19 @@ class Calculator:
     def pangkat(self):
         return self.angka1**self.angka2
 
+    def module(self):
+        return self.angka1 % self.angka2
+
     def result(self):
         func = [
-          (self.add(), 'Penjumlahan'),
-          (self.substract(), 'Pengurangan'), 
-          (self.multiply(), 'Perkalian'),
-          (self.divide(), 'Pembagian'),
- 	  (self.pangkat(), 'Perpangkatan')
+            (self.add(), 'Penjumlahan'),
+            (self.substract(), 'Pengurangan'),
+            (self.multiply(), 'Perkalian'),
+            (self.divide(), 'Pembagian'),
+            (self.pangkat(), 'Perpangkatan')
         ]
         hasil = []
         for fun, desc in func:
-            hasil.append("Hasil dari {} {} dan {} = {}".format(desc, self.angka1, self.angka2, fun))
+            hasil.append("Hasil dari {} {} dan {} = {}".format(
+                desc, self.angka1, self.angka2, fun))
         return hasil
-
